cdf6ebb87ea7ffd82de4575547c76e4127dd56a0
florixak <florixakcz@seznam.cz>
2023-02-13 07:55:58 +0100

    Start settings

---
+++ NinjaWay/Assets/Scripts/SettingsManager.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SettingsManager : MonoBehaviour
+{
+    public float musicVolume = 10f;
+    public float soundVolume = 10f;
+
+    
+}
